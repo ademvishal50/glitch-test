dfc0f7975c771cb933a8bd983799d39a65caf575
vishal <ademvishal50@gmail.com>
2023-06-02 20:06:29 +0530

    initial

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
-
-from datetime import datetime
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html", now=datetime.now())
+	return jsonify({"msg":"hello deploy from glitch"})
 
 if __name__ == "__main__":
 	app.run()
